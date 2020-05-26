6f7bc414fff0f573925f43484f629a712334fb69
Chris <free2737@gmail.com>
2020-05-26 15:10:06 +0800

    update code

--- dist/script.js
+++ dist/script.js
@@ -1,17 +1,17 @@
-  (function () {
-    var shareButtons = document.querySelectorAll(".share-btn");
+(function () {
+  var shareButtons = document.querySelectorAll(".share-btn");
 
-    if (shareButtons) {
-      [].forEach.call(shareButtons, function (button) {
-        button.addEventListener("click", function (event) {
-          var width = 650,
-            height = 450;
+  if (shareButtons) {
+    [].forEach.call(shareButtons, function (button) {
+      button.addEventListener("click", function (event) {
+        var width = 650,
+          height = 450;
 
-          event.preventDefault();
+        event.preventDefault();
 
-          window.open(this.href, 'Share Dialog', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,width=' + width + ',height=' + height + ',top=' + (screen.height / 2 - height / 2) + ',left=' + (screen.width / 2 - width / 2));
-        });
+        window.open(this.href, 'Share Dialog', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,width=' + width + ',height=' + height + ',top=' + (screen.height / 2 - height / 2) + ',left=' + (screen.width / 2 - width / 2));
       });
-    }
+    });
+  }
 
-  })();
+})();
