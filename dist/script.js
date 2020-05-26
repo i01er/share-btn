d16dacb42bc2f28b3ca4471babdb7994cc2e85ae
Chris <free2737@gmail.com>
2020-05-26 14:43:45 +0800

    first commit

---
+++ dist/script.js
@@ -0,0 +1,30 @@
+var app = angular.module("myApp", []);
+
+app.config(function($compileProvider){
+  //other configuration code here
+  $compileProvider.aHrefSanitizationWhitelist(/^\s*(whatsapp):/);
+});
+
+app.controller("myCtrl", function ($scope) {
+  $scope.title = "Argaam Article";
+  $scope.website = "https://www.argaam.com/en";
+
+  (function () {
+    var shareButtons = document.querySelectorAll(".share-btn");
+
+    if (shareButtons) {
+      [].forEach.call(shareButtons, function (button) {
+        button.addEventListener("click", function (event) {
+          var width = 650,
+            height = 450;
+
+          event.preventDefault();
+
+          window.open(this.href, 'Share Dialog', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,width=' + width + ',height=' + height + ',top=' + (screen.height / 2 - height / 2) + ',left=' + (screen.width / 2 - width / 2));
+        });
+      });
+    }
+
+  })();
+
+});
